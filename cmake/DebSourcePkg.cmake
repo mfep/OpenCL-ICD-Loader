36e674330648c472a48fa9c123dc0733d95cbed4
Lőrinc Serfőző <lorinc@streamhpc.com>
2023-09-27 15:03:19 +0200

    WIP

--- cmake/DebSourcePkg.cmake
+++ cmake/DebSourcePkg.cmake
@@ -114,7 +114,7 @@ file(WRITE "${DEB_SOURCE_PKG_DIR}/rules"
 override_dh_auto_configure:
 \tdh_auto_configure -- -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF
 
-GENERATED_MAINTAINER_SCRIPTS := $(patsubst %.in,%,$(wildcard debian/*.postinst.in debian/*.prerm.in))
+GENERATED_MAINTAINER_SCRIPTS := $(patsubst %.in,%,$(wildcard debian/*.alternatives.in))
 
 $(GENERATED_MAINTAINER_SCRIPTS): %: %.in
 \tsed \"s%@DEB_HOST_MULTIARCH@%$(DEB_HOST_MULTIARCH)%g\" < $< > $@
@@ -130,12 +130,12 @@ usr/share
 file(WRITE "${DEB_SOURCE_PKG_DIR}/${CPACK_DEBIAN_RUNTIME_PACKAGE_NAME}.install"
 "usr/lib/*/lib*.so.* usr/lib/\${DEB_HOST_MULTIARCH}/KhronosOpenCLICDLoader
 ")
-file(WRITE "${DEB_SOURCE_PKG_DIR}/${CPACK_DEBIAN_RUNTIME_PACKAGE_NAME}.alternatives"
-"Name: libOpenCL.so.1.0.0-x86_64-linux-gnu
-Link: /usr/lib/x86_64-linux-gnu/libOpenCL.so.1.0.0
-Alternative: /usr/lib/x86_64-linux-gnu/KhronosOpenCLICDLoader/libOpenCL.so.1.0.0
+file(WRITE "${DEB_SOURCE_PKG_DIR}/${CPACK_DEBIAN_RUNTIME_PACKAGE_NAME}.alternatives.in"
+"Name: libOpenCL.so.1.0.0-@DEB_HOST_MULTIARCH@
+Link: /usr/lib/@DEB_HOST_MULTIARCH@/libOpenCL.so.1.0.0
+Alternative: /usr/lib/@DEB_HOST_MULTIARCH@/KhronosOpenCLICDLoader/libOpenCL.so.1.0.0
 Dependents:
-  /usr/lib/x86_64-linux-gnu/libOpenCL.so.1 libOpenCL.so.1-x86_64-linux-gnu /usr/lib/x86_64-linux-gnu/KhronosOpenCLICDLoader/libOpenCL.so.1
+  /usr/lib/@DEB_HOST_MULTIARCH@/libOpenCL.so.1 libOpenCL.so.1-@DEB_HOST_MULTIARCH@ /usr/lib/@DEB_HOST_MULTIARCH@/KhronosOpenCLICDLoader/libOpenCL.so.1
 Priority: 100
 ")
 file(WRITE "${DEB_SOURCE_PKG_DIR}/${CPACK_DEBIAN_CLLAYERINFO_PACKAGE_NAME}.install"
